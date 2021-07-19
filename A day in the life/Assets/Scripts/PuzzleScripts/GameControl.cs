b17bd9d36d8c9f57d8ceae0d034790d327dfc9df
CooperSpyrou <74168780+CooperSpyrou@users.noreply.github.com>
2021-07-19 14:04:53 +1000

    First Puzzle Completed

---
+++ A day in the life/Assets/Scripts/PuzzleScripts/GameControl.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] pictures;
+
+    public static bool youWin;
+
+    void Start () 
+    {
+        youWin = false;
+    }
+
+    void Update () 
+    {
+        if (pictures[0].rotation.z == 0 &&
+            pictures[1].rotation.z == 0 &&
+            pictures[2].rotation.z == 0 &&
+            pictures[3].rotation.z == 0 &&
+            pictures[4].rotation.z == 0 &&
+            pictures[5].rotation.z == 0 &&
+            pictures[6].rotation.z == 0 &&
+            pictures[7].rotation.z == 0 &&
+            pictures[8].rotation.z == 0 &&
+            pictures[9].rotation.z == 0 &&
+            pictures[10].rotation.z == 0 &&
+            pictures[11].rotation.z == 0 &&
+            pictures[12].rotation.z == 0 &&
+            pictures[13].rotation.z == 0 &&
+            pictures[14].rotation.z == 0) 
+        {
+            youWin = true;
+        }
+    }
+}
